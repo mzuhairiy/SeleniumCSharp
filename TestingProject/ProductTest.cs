8246d5c8a2a85336ab4d23bfd383b63fa54da068
Zuhair <zuhairimuhammad13@gmail.com>
2024-01-24 20:49:32 +0700

    * ProductTest.cs: add ProductTest.cs
    * LoginTest.cs: remove TestCase3

---
+++ TestingProject/ProductTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TestingProject
+{
+    [TestFixture]
+    public class ProductTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+    }
+}
